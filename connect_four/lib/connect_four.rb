30f747b23662b1f9116c2c0f196f8701831df423
Saul Belisle <rbelisle1270@gmail.com>
2020-05-25 14:55:34 -0700

    finished project, w/ test specs

--- connect_four/lib/connect_four.rb
+++ connect_four/lib/connect_four.rb
@@ -2,8 +2,8 @@ require_relative 'players.rb'
 
 class Connect_Four
     attr_accessor :board_spaces, :header_row, :board, :players, :game_over
-    black = "\u25CF"
-    white = "\u26AA"
+    # black = "\u25CF"
+    # white = "\u26AA"
 
     def initialize
         @board = Array.new(7) {Array.new(7, "\u23FA")}
@@ -35,7 +35,8 @@ class Connect_Four
         puts "#{player.name} choose a column number to drop a '#{player.team}''"
         spot = gets.chomp.to_i 
         spot = spot - 1
-            if check_column(spot, player) == true
+        options = [0,1,2,3,4,5,6]
+            if options.include?(spot) && check_column(spot, player) == true
                 return spot
             else
                 get_input(player)
@@ -61,11 +62,21 @@ class Connect_Four
         end
     end
 
-    def check_winner(player)
+    def gets_row(row = 6, col, player)
+        if board[row][col] == "\u23FA"
+            return row
+        else
+            gets_row(row - 1, col, player)
+        end
+    end
+
+    def check_winner(player, row, spot)
             if 
                 check_horizontal_winner(player)
             elsif 
                 check_vertical_winner(player)
+            elsif
+                check_diagonal_winner(player, row, spot)
             else 
                 check_for_tie
             end
@@ -83,17 +94,51 @@ class Connect_Four
     end
 
     def check_vertical_winner(player)
-
-        p vertical_array
-        # vertical_array.each{|col|
-        #     if col.each_cons(4).any? {|a,b,c,d| a == "#{player.team}" && b == "#{player.team}" && c == "#{player.team}" && d == "#{player.team}" }
-        #     puts "congrats, #{player.name}! You Win!"
-        #     @game_over = true
-        #     return true
-        #     end
-        # }   
-        # return false
+        a = 0
+        b = 0
+        all_vertical_arrays = []
+        vertical_array = []
+        6.times do 
+            7.times do
+                vertical_array << board[a][b]
+                a += 1
+            end
+                all_vertical_arrays << vertical_array
+                b += 1
+                a = 0
+        end
+            all_vertical_arrays.each{|col|
+                if col.each_cons(4).any? {|a,b,c,d| a == "#{player.team}" && b == "#{player.team}" && c == "#{player.team}" && d == "#{player.team}" }
+                    puts "congrats, #{player.name}! You Win!"
+                    @game_over = true
+                    return true
+                end
+                }
+        return false
     end
+
+    def check_diagonal_winner(player, row, spot)
+        n = (row * 7) + spot
+        flat = board.flatten
+            if flat[n] == player.team && flat[n + 6] == player.team && flat[n + 12] == player.team && flat[n + 18] == player.team 
+                puts "congrats, #{player.name}! You Win!"
+                @game_over = true
+                    return true
+            elsif flat[n] == player.team && flat[n - 6] == player.team && flat[n - 12] == player.team && flat[n - 18] == player.team 
+                puts "congrats, #{player.name}! You Win!"
+                @game_over = true
+                    return true
+            elsif flat[n] == player.team && flat[n + 8] == player.team && flat[n + 16] == player.team && flat[n + 24] == player.team 
+                puts "congrats, #{player.name}! You Win!"
+                @game_over = true
+                    return true
+            elsif flat[n] == player.team && flat[n - 8] == player.team && flat[n - 16] == player.team && flat[n - 24] == player.team 
+                puts "congrats, #{player.name}! You Win!"
+                @game_over = true
+                    return true
+            end
+            return false
+        end
 
     def check_for_tie
         new_board = board.flatten
@@ -105,9 +150,10 @@ class Connect_Four
 
     def take_turn(player)
         spot = self.get_input(player)
+        row = self.gets_row(spot, player)
         self.update_board(spot, player)
         self.print_board
-        self.check_winner(player)
+        self.check_winner(player, row, spot)
     end
 
     def play_game
--- connect_four/spec/connect_four_spec.rb
+++ connect_four/spec/connect_four_spec.rb
@@ -3,70 +3,123 @@ require './lib/connect_four.rb'
 describe Connect_Four do
 
     describe "#print_board" do
-            it "prints the board to the terminal" do
+        it "prints the board to the terminal" do
             game = Connect_Four.new 
             output = game.print_board
-            expect(output).to eql(
-                [[1, 2, 3, 4, 5, 6, 7],
-                ["▢", "▢", "▢", "▢", "▢", "▢", "▢"],
-                ["▢", "▢", "▢", "▢", "▢", "▢", "▢"],
-                ["▢", "▢", "▢", "▢", "▢", "▢", "▢"],
-                ["▢", "▢", "▢", "▢", "▢", "▢", "▢"],
-                ["▢", "▢", "▢", "▢", "▢", "▢", "▢"],
-                ["▢", "▢", "▢", "▢", "▢", "▢", "▢"]])
-            end
+        expect(game.board).to eql(
+            [[1, 2, 3, 4, 5, 6, 7],
+            ["\u23FA", "\u23FA", "\u23FA", "\u23FA", "\u23FA", "\u23FA", "\u23FA"],
+            ["\u23FA", "\u23FA", "\u23FA", "\u23FA", "\u23FA", "\u23FA", "\u23FA"],
+            ["\u23FA", "\u23FA", "\u23FA", "\u23FA", "\u23FA", "\u23FA", "\u23FA"],
+            ["\u23FA", "\u23FA", "\u23FA", "\u23FA", "\u23FA", "\u23FA", "\u23FA"],
+            ["\u23FA", "\u23FA", "\u23FA", "\u23FA", "\u23FA", "\u23FA", "\u23FA"],
+            ["\u23FA", "\u23FA", "\u23FA", "\u23FA", "\u23FA", "\u23FA", "\u23FA"]])
         end
+    end
 
     describe "#set_up" do
         it "puts both players in a array" do
             game = Connect_Four.new
             player_1 = Player.new("Jack", "Black")
             player_2 = Player.new("Jill", "white")
-            expect(game.set_up(player_1, player_2)).to eql([player_1, player_2])
+            game.set_up(player_1, player_2)
+        expect(game.players).to eql([player_1, player_2])
         end
     end
 
-    # describe "#get_input" do
-    #     it "returns a column from the user" do
-    #         game = Connect_Four.new
-    #         player_1 = Player.new("Jack", "Black")
-    #         player_2 = Player.new("Jill", "white")
-    #         expect(game.get_input(player_1)).to eql(true)
-    #     end
-    # end
-
-    describe "#take_turn" do
-        it "adds a circle to the correct column chosen" do
+    describe "#get_input" do
+    it "accepts a column from the user" do
             game = Connect_Four.new
             player_1 = Player.new("Jack", "Black")
             player_2 = Player.new("Jill", "white")
-            expect(take_turn(player_1)).to eql(
-            [[1, 2, 3, 4, 5, 6, 7],
-            ["▢", "▢", "▢", "▢", "▢", "▢", "▢"],
-            ["▢", "▢", "▢", "▢", "▢", "▢", "▢"],
-            ["▢", "▢", "▢", "▢", "▢", "▢", "▢"],
-            ["▢", "▢", "▢", "▢", "▢", "▢", "▢"],
-            ["▢", "▢", "▢", "▢", "▢", "▢", "▢"],
-            ["▢", "Black", "▢", "▢", "▢", "▢", "▢"]]
-            )
+            game.stub(:gets) {"3\n"}
+        expect(game.get_input(player_1)).to eql(2)
         end
     end
 
     describe "#check_column" do
-    it "verifies there is an open space in the column" do
-        game = Connect_Four.new
-        player_1 = Player.new("Jack", "Black")
-        player_2 = Player.new("Jill", "white")
-        board =  [[1, 2, 3, 4, 5, 6, 7],
-        ["▢", "▢", "▢", "▢", "▢", "▢", "▢"],
-        ["▢", "▢", "▢", "▢", "▢", "▢", "▢"],
-        ["▢", "▢", "▢", "▢", "▢", "▢", "▢"],
-        ["▢", "▢", "▢", "▢", "▢", "▢", "▢"],
-        ["▢", "▢", "▢", "▢", "▢", "▢", "▢"],
-        ["▢", "Black", "▢", "▢", "▢", "▢", "▢"]]
-        expect(game.check_column(2)).to eql(2)
+    it "it verifies a column is not full" do
+            game = Connect_Four.new
+            player_1 = Player.new("Jack", "Black")
+            player_2 = Player.new("Jill", "white")
+            game.board[6][4] = "\u23FA"
+        expect(game.check_column(4, player_1)).to eql(true)
+        end
     end
-end
+
+    describe "#update_board" do
+    it "it updates the board with the players team" do
+            game = Connect_Four.new
+            player_1 = Player.new("Jack", "Black")
+            player_2 = Player.new("Jill", "white")
+            game.board[6][4] = "\u23FA"
+            game.update_board(4, player_1)
+        expect(game.board[6][4]).to eql(player_1.team)
+        end
+    end
+
+    describe "#gets_row" do
+    it "it returns the row the piece lands in" do
+            game = Connect_Four.new
+            player_1 = Player.new("Jack", "Black")
+            player_2 = Player.new("Jill", "white")
+            game.board[6][4] = player_2.team
+        expect(game.gets_row(4, player_1)).to eql(5)
+        end
+    end
+
+    describe "#check_horizontal_winner" do
+        it "it checks for winner horizontally" do
+            game = Connect_Four.new
+            player_1 = Player.new("Jack", "Black")
+            player_2 = Player.new("Jill", "white")
+            game.board[6][3] = player_1.team
+            game.board[6][4] = player_1.team
+            game.board[6][5] = player_1.team
+            game.board[6][6] = player_1.team
+            game.check_horizontal_winner(player_1)
+        expect(game.game_over).to eql(true)
+        end
+    end
+
+    describe "#check_vertical_winner" do
+        it "it checks for winner vertically" do
+            game = Connect_Four.new
+            player_1 = Player.new("Jack", "Black")
+            player_2 = Player.new("Jill", "white")
+            game.board[6][4] = player_1.team
+            game.board[5][4] = player_1.team
+            game.board[4][4] = player_1.team
+            game.board[3][4] = player_1.team
+            game.check_vertical_winner(player_1)
+        expect(game.game_over).to eql(true)
+        end
+    end
+
+    describe "#check_diagonal_winner" do
+        it "it checks for winner diagonally" do
+            game = Connect_Four.new
+            player_1 = Player.new("Jack", "Black")
+            player_2 = Player.new("Jill", "white")
+            game.board[6][4] = player_1.team
+            game.board[5][3] = player_1.team
+            game.board[4][2] = player_1.team
+            game.board[3][1] = player_1.team
+            game.check_diagonal_winner(player_1, 3, 1)
+        expect(game.game_over).to eql(true)
+        end
+    end
+
+    describe "#check_for_tie" do
+        it "it checks for a tie scenario" do
+            game = Connect_Four.new
+            player_1 = Player.new("Jack", "Black")
+            player_2 = Player.new("Jill", "white")
+            game.board.map!{|a| a = "test"}
+            game.check_for_tie
+        expect(game.game_over).to eql(true)
+        end
+    end
 
 
 end
